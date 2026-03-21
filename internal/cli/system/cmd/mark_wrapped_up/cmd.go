2f7d87292850b81ec7229c89e7ef6a944a68df52
Jose Alekhinne <jose@ctx.ist>
2026-03-21 10:42:58 -0700

    Fix mark_journal run.go: cflag.Check, token.CommaSpace
    Signed-off-by: Jose Alekhinne <jose@ctx.ist>

--- internal/cli/system/cmd/mark_wrapped_up/cmd.go
+++ internal/cli/system/cmd/mark_wrapped_up/cmd.go
@@ -7,9 +7,10 @@
 package mark_wrapped_up
 
 import (
+	"github.com/spf13/cobra"
+
 	"github.com/ActiveMemory/ctx/internal/assets/read/desc"
 	"github.com/ActiveMemory/ctx/internal/config/embed/cmd"
-	"github.com/spf13/cobra"
 )
 
 // Cmd returns the "ctx system mark-wrapped-up" subcommand.
